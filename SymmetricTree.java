9db1f4903dd6a76602e60811176762dec4ef6f97
y11301111 <599243189@qq.com>
2018-08-17 11:53:16 +0800

    101. 对称二叉树

---
+++ SymmetricTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root == null){
+            return true;
+        }
+        return compared(root.right,root.left);
+    }
+    
+    public boolean compared(TreeNode r1,TreeNode r2) {
+        if(r1 == null && r2 == null){
+            return true;
+        }
+        if(r1 == null || r2 == null) {
+            return false;
+        }
+         if(r1.val == r2.val){
+            return compared(r1.left,r2.right) &&  compared(r1.right,r2.left);
+        }
+        return false;
+    }
+}
