67aeafba00d8f879719c87315afa6bd6509a2065
y11301111 <599243189@qq.com>
2018-08-23 18:19:47 +0800

    对称树，相同树

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -29,3 +29,27 @@ class Solution {
         return false;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.right,q.right) && isSameTree(p.left,q.left);
+        }
+        return false;
+    }
+}
